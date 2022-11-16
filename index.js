fa260d23ec7d137a3505f429aff21dec22f1d6f6
Mahbub2001 <imahbubturza@gmail.com>
2022-11-16 16:43:02 +0600

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
